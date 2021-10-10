77097958f6398d48497a4892c0554ff419749cac
Alekh Kumar <alekhkumar777@gmail.com>
2021-10-11 01:49:14 +0530

    Send and receive message

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const http = require('http')
 const express = require('express')
 const app = express();
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const { SocketAddress } = require('net');
 
 const server = http.createServer(app)
 const io = socketio(server)
 
 io.on('connection', (socket)=>{
-    
+    console.log('connected with socket id = ', socket.id);
+
+    socket.on('msg_send', (data)=>{
+        console.log('received message = ', data.msg)
+        io.emit('msg_rcvd', data); // we are not doing socket.emit, because it only emits the "msg_rcvd" to only one socket which 
+                                   // invoked this "msg_send" function. "io.emit" on the other hand, emits to every socket on "io".
+                                   // We can also write "socket.broadcast.emit" to emit to every socket but it doesn't emit to the
+                                   // current socket.
+                                   // 3 options
+                                   // 1. socket.emit -> emits to current socket only.
+                                   // 2. io.emit -> emits to every socket including the current one.
+                                   // 3. socket.broadcast.emit -> emits to every socket except the current one.
+    })
 })
 
 app.use('/', express.static(__dirname + '/public'))
